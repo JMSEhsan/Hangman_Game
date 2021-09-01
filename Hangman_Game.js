c699d79d120f0c6ad0b912c55c70ad0fb1945ccf
JMSEhsan <JMSEhsan@gmail.com>
2021-09-01 01:25:12 -0600

    commit

--- Hangman_Game.js
+++ Hangman_Game.js
@@ -1,2 +1,12 @@
-// Hangman Game
+// Hangman Game using function
+
+// create an array of words
+
+var words = ["javascript", "monkey", "banana", "amazing", "apple"];
+
+var pickWord = function(){
+    return words[Math.floor(words.length*Math.random())];
+};
+
+console.log(pickWord());
 
